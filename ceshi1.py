a5f6c3dad23f370ab5e74745f6459be02f504aca
root <root@localhost.localdomain>
2018-09-25 09:08:07 -0400

    test

---
+++ ceshi1.py
@@ -0,0 +1,79 @@
+#1.
+#c =float( raw_input(">>enter a degree in celsius:"))
+#f =((9.0/5.0)*c+32)
+#print(f)
+
+
+
+#2.
+
+#r = float(raw_input(">>"))
+#l =float( raw_input(">>"))
+#a = r*r*3.14
+#v = a*l
+#print(a,v)
+
+
+#3.
+#y = float(raw_input(">>enter a value for feet:"))
+#m = y*0.305
+#print(m)
+
+
+#4.
+#m = float(raw_input(">>enter the amount of wanter in ki:"))
+#it = float(raw_input(">>enter the i t :"))
+#t = float(raw_input(">>enter the final t:"))
+#q = m*(t-it)*4184
+#print(q)
+
+
+
+
+
+#5.
+#c = float(raw_input(">>enter balance:"))
+#nll = float(raw_input(">>enter interest rate:"))
+#l = c*(nll/1200)
+#print('%5f'%(l))
+
+
+
+#6.
+#v0 = float(raw_input(">>enter v0:"))
+#v = float(raw_input(">>enter v:"))
+#t = float(raw_input(">>enter t:"))
+#a = (v-v0)/t
+#print('%.4f'%(a))
+
+
+#7.
+m = float(raw_input(">>enter tne mon sav amount:"))
+a = 0.05/12
+q = 1+a
+b = m*q
+c = (m+b)*q
+d = (m+c)*q
+e = (m+d)*q
+f = (m+e)*q
+g = (m+f)*q
+print('%.2f'%(g))
+
+
+
+
+#8.
+
+#a = int(raw_input(">>enter num between 0 and 1000:"))
+#b = a/100
+#c = a/10%10
+#d = a%10
+#sum = b+c+d
+#print(sum)
+
+
+
+
+
+
+
