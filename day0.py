adb04efac709dd013ad34be47b2640f5348821d0
root <root@localhost.localdomain>
2018-09-26 03:59:00 -0400

    test

---
+++ day0.py
@@ -0,0 +1,55 @@
+#a = 'welcome '
+#b = 'to ' 
+#c = 'python'
+#d = a+b+c
+#print(d)
+
+
+
+1.
+#a = eval(raw_input(">>"))
+#if int(a % 2) == 0:
+ #       print('ou')
+#else:
+ #       print('ji')
+
+'''
+
+2.
+print('start')
+res = raw_input("shuaifou?y/n:>>")
+if res=='y':
+    res2 = raw_input("youqianfou?y/n:>>")
+    if res2 =='y':
+        res3 =raw_input("youlaopofou?y/n:>>")
+        if res3 == 'n':
+            res4 = raw_input("youfangfou?y/n:>>")
+            if res4 == 'y':
+                print('see see' )
+            else:
+                print('zaijian')
+        else:
+            print('bujian')
+    else:
+        print('gun')
+else:
+    print('gun')
+'''
+
+'''
+import random
+num = int(raw_input(">>"))
+num2 = random.randrange(0,5)
+if num > num2:
+    print('lager')
+elif num < num2:
+    print('small')
+elif num == num2:
+    print('currect')
+'''
+
+
+
+
+
+
